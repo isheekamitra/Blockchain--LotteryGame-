56155589c12b24c7a290b8d6a197746ef0ab7082
isheekamitra <2019ugee003@nitjsr.ac.in>
2021-10-11 20:16:48 +0530

    adding ethereum

---
+++ lottery/test/Lottery.test.js
@@ -0,0 +1,94 @@
+const assert=require('assert');
+const ganache=require('ganache-cli');
+const Web3=require('web3');
+const web3=new Web3(ganache.provider());
+const {interface,bytecode}=require('../compile');
+
+let lottery;
+let accounts;
+
+beforeEach(async()=>{
+    accounts=await web3.eth.getAccounts();
+    lottery= await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+});
+
+describe ('Lottery Contract',()=>{
+    it('deploys a contract',()=>{
+        assert.ok(lottery.options.address);
+    });
+
+    it('allows entry of one person',async()=>{
+        await lottery.methods.enter().send({
+            from:accounts[0],
+            value: web3.utils.toWei('0.02','ether')
+        });
+        const players=await lottery.methods.getPlayers().call({
+            from:accounts[0]
+        })
+        assert.equal(accounts[0],players[0]);
+        assert.equal(1,players.length);
+    });
+
+    it('allows entry of multiple persons',async()=>{
+        await lottery.methods.enter().send({
+            from:accounts[0],
+            value: web3.utils.toWei('0.02','ether')
+        });
+        await lottery.methods.enter().send({
+            from:accounts[1],
+            value: web3.utils.toWei('0.02','ether')
+        });
+        await lottery.methods.enter().send({
+            from:accounts[2],
+            value: web3.utils.toWei('0.02','ether')
+        });
+        const players=await lottery.methods.getPlayers().call({
+            from:accounts[0]
+        })
+        assert.equal(accounts[0],players[0]);
+        assert.equal(accounts[1],players[1]);
+        assert.equal(accounts[2],players[2]);
+        assert.equal(3,players.length);
+    });
+    
+
+    it('requires min ether to enter',async()=>{
+        try{
+        await lottery.methods.enter().send({
+            from:accounts[0],
+            value:0
+        });
+        assert(false);
+    }catch(err){
+        assert(err);
+    }
+    });
+
+    it('ónly manager can call',async()=>{
+        try{
+            await lottery.methods.pickWinner().send({
+                from:accounts[1]
+            });
+          assert(false);
+        }
+    catch(err){
+        assert(err);
+    }
+    });
+    it('sends money to winner and resets array',async()=>{
+    
+            await lottery.methods.enter().send({
+                from:accounts[0],
+                value: web3.utils.toWei('2','ether')
+            });
+      
+    const initialbalance=await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({from:accounts[0]});
+    const finalbalance=await web3.eth.getBalance(accounts[0]);
+    const diff=finalbalance-initialbalance;
+    assert(diff>web3.utils.toWei('1.8','ether'));
+
+    });
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import web3 from './web3';
+import lottery from "./lottery";
+class App extends React.Component {
+state={
+  manager:'',
+  players:[],
+  balance:'',
+  value:'',
+  message:''
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+ async componentDidMount(){
+    const manager= await lottery.methods.manager().call();
+    const players= await lottery.methods.getPlayers().call();
+    const balance= await web3.eth.getBalance(lottery.options.address)
+    this.setState({manager:manager ,players:players,balance:balance});
+  }
+  onSubmit=async(event)=>{
+    event.preventDefault();
+
+    const accounts=await web3.eth.getAccounts();
+
+
+  this.setState({message:'Working on giving you entry to the pool...Please wait for few moments!'})
+    await lottery.methods.enter().send({
+      from:accounts[0],
+      value:web3.utils.toWei(this.state.value,'ether')
+    });
+    this.setState({message:'Congratulations!! You are now a part of the pool.'})
+  };
+  winner=async(event)=>{
+    const accounts=await web3.eth.getAccounts();
+
+
+    this.setState({message:'Deciding Winner...'})
+      await lottery.methods.pickWinner().send({
+        from:accounts[0],
+      
+      });
+      this.setState({message:'Winner Declared!!'})
+  }
+  render() {
+    return (
+      
+      <div style={{textAlign:"center"}}>
+        <h2 style={{color:'hotpink'}}>Lottery Game!!</h2>
+        <p style={{color:'indigo'}}>This contract is managed by {this.state.manager} <br/> There are currently {this.state.players.length} number of people
+        competing in our game to with an amount of {web3.utils.fromWei(this.state.balance,'ether')} ether ! 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+        <hr/>
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck!! Wohhooo good luck :)</h4>
+          <label style={{color:'indigo'}}>Amount of ether you wanna put into the game to enter</label>
+          
+          <div>
+          <br/>
+          <input
+          value={this.state.value}
+            onChange={event=>this.setState({value:event.target.value})}
+          />
+        
+          </div>
+          <br/>
+          <button style={{margin:'10px',padding:'10px',background:'pink'}}>click to Enter </button>
+        </form>
+        <hr/>
+        <h4>Ready to pick a winner?</h4> <p>(Only for manager and office use)</p>
+        <button onClick={this.winner} style={{margin:'10px',padding:'10px',background:'pink'}}>Pick THE Winneeer Winnerrr!!!</button>
+        <h1 >{this.state.message}</h1>
+      </div>
+    
+    );
+  }
+}
 export default App;
+
